9a9f222521a215ad7c48655da78d11aa40cf3fd8
bdu <9451823+b-du@users.noreply.github.com>
2020-10-17 01:04:41 +0200

    First tests (#1)
    first working (hopefully) version

---
+++ bot.py
@@ -0,0 +1,98 @@
+import discord
+from discord.ext import commands
+import asyncio
+import inscriptions
+import configparser
+import random
+import datetime
+
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8')
+config_api = config['api_server']
+config_discord = config['discord']
+config_cron = config['cron']
+
+api_url_base = config_api['api_url_base']
+api_token = config_api['api_token']
+api_days_check_min = config_api['days_check_min']
+api_days_check_max = config_api['days_check_max']
+discord_token = config_discord['discord_token']
+discord_server = config_discord['server']
+discord_channel = config_discord['channel']
+discord_icon = config_discord['icon']
+discord_messages = config_discord['messages'].split('\n')
+discord_images = config_discord['images'].split('\n')
+cron_hours = int(config_cron['hours'])
+cron_minutes = int(config_cron['minutes'])
+
+
+class BdwBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # create the background task and run it in the... background.
+        self.bg_task = self.loop.create_task(self.background_task())
+
+    def find_user(self, nick):
+        for guild in self.guilds:
+            user = next((m for m in guild.members if str.lower(nick) in str.lower(m.display_name)), None)
+            if user is not None:
+                return user
+
+        return None
+
+    async def check_signed_in_users_for_next_raid(self, check_period):
+        guild = next((g for g in self.guilds if g.name == discord_server), None)
+        if guild is not None:
+            channel = next((c for c in guild.channels if c.name == discord_channel), None)
+            if channel is not None:
+                next_raid_inscriptions = inscriptions.check_next_raid_inscriptions(api_url_base,
+                                                                                   api_token,
+                                                                                   check_period,
+                                                                                   api_days_check_min,
+                                                                                   api_days_check_max)
+                not_checked_in_users = next_raid_inscriptions['not_checked_in_users']
+                if len(not_checked_in_users) > 0:
+                    message = ''
+                    for not_checked_in_user in not_checked_in_users:
+                        user = self.find_user(not_checked_in_user)
+                        if user is not None:
+                            message += '- ' + user.mention + '\n'
+                        else:
+                            message += '- ' + not_checked_in_user + '\n'
+                    embed = discord.Embed(title='Raid du ' + next_raid_inscriptions['date'],
+                                          description=random.choice(discord_messages), color=0x309bf3)
+                    embed.set_thumbnail(url=discord_icon)
+                    embed.set_image(url=random.choice(discord_images))
+                    embed.add_field(name='Non inscrits:', value=message, inline=False)
+                    await channel.send(embed=embed)
+
+    async def background_task(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            now = datetime.datetime.now()
+            if now.hour == cron_hours and now.minute == cron_minutes:
+                await self.check_signed_in_users_for_next_raid(True)
+            await asyncio.sleep(60)
+
+
+description = '''Bot inscription BdW'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = BdwBot(command_prefix='!', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+
+@bot.command()
+async def check_next_raid(context):
+    await context.message.delete()
+    await bot.check_signed_in_users_for_next_raid(False)
+
+
+bot.run(discord_token)
---
+++ inscriptions.py
@@ -0,0 +1,88 @@
+import requests
+import xml.etree.ElementTree as etree
+import datetime
+
+FUNCTION_GET_EVENTS = 'calevents_list'
+FUNCTION_GET_DETAILED_EVENT= 'calevents_details'
+FUNCTION_GET_ALL_CHARACTERS = 'points'
+
+
+def build_request(base, function, api_token, *opts):
+    request = base + '?function=' + function
+    for opt in opts:
+        request += '&' + opt['key'] + '=' + opt['value']
+
+    request += '&atoken=' + api_token + '&atype=api'
+    return request
+
+
+def get_next_raid(api_url_base, api_token):
+    api_url = build_request(api_url_base, FUNCTION_GET_EVENTS, api_token, {'key': 'raids_only', 'value': '1'}, {'key': 'number', 'value': '3'})
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        raids = etree.fromstring(response.content.decode('utf-8').replace('\n', ''))
+        for raid in raids.find('events'):
+            raid_start_timestamp = int(raid.find('start_timestamp').text)
+            if (datetime.datetime.fromtimestamp(raid_start_timestamp)) > datetime.datetime.now():
+                return raid
+
+    return None
+
+
+def get_raid_detailed_info(raid_id, api_url_base, api_token):
+    api_url = build_request(api_url_base, FUNCTION_GET_DETAILED_EVENT, api_token, {'key': 'eventid', 'value': raid_id})
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        detailed_raid = etree.fromstring(response.content.decode('utf-8').replace('\n', ''))
+        return detailed_raid
+
+    return None
+
+
+def get_main_active_chars(api_url_base, api_token):
+    api_url = build_request(api_url_base, FUNCTION_GET_ALL_CHARACTERS, api_token)
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        main_active_chars = {}
+        all_chars = etree.fromstring(response.content.decode('utf-8').replace('\n', ''))
+        for char in all_chars.find('players'):
+            id = char.find('id').text
+            main_id = char.find('main_id').text
+            if id == main_id and char.find('active').text == '1':
+                main_active_chars[int(id)] = char.find('name').text
+
+        return main_active_chars
+
+    return None
+
+
+def get_not_signed_in_users(detailed_raid, main_active_chars):
+    not_signed_in = dict(main_active_chars)
+    for user in detailed_raid.find('raidstatus').iter('char'):
+        user_id = int(user.find('id').text)
+        if not_signed_in.get(user_id, None) is not None:
+            del not_signed_in[user_id]
+
+    return not_signed_in.values()
+
+
+def check_next_raid_inscriptions(api_url_base, api_token, check_period, days_check_min=1, days_check_max=2):
+    next_raid = get_next_raid(api_url_base, api_token)
+    if next_raid is not None:
+        next_raid_start_timestamp = int(next_raid.find('start_timestamp').text)
+        next_raid_start_datetime = datetime.datetime.fromtimestamp(next_raid_start_timestamp)
+
+        # Is next raid in less than x days and more than y?
+        if not check_period \
+                or ((next_raid_start_datetime - datetime.timedelta(days=int(days_check_min))) < datetime.datetime.now()
+                    < (next_raid_start_datetime - datetime.timedelta(days=int(days_check_max)))):
+            next_raid_id = next_raid.find('eventid').text
+            det_raid = get_raid_detailed_info(next_raid_id, api_url_base, api_token)
+            main_active_chars = get_main_active_chars(api_url_base, api_token)
+            return {'date': next_raid_start_datetime.strftime('%d/%m/%Y'),
+                    'not_checked_in_users': get_not_signed_in_users(det_raid, main_active_chars)}
+
+    return None
