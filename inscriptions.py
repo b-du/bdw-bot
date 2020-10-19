ef0fdd11924537ac6f49c42bf8eb9a00854ca5eb
bdu <bdu@bdu.ndu>
2020-10-19 20:50:45 +0200

    Fix problem with multiple next raids before the time period

--- inscriptions.py
+++ inscriptions.py
@@ -16,16 +16,19 @@ def build_request(base, function, api_token, *opts):
     return request
 
 
-def get_next_raid(api_url_base, api_token):
-    api_url = build_request(api_url_base, FUNCTION_GET_EVENTS, api_token, {'key': 'raids_only', 'value': '1'}, {'key': 'number', 'value': '3'})
+def get_next_raids(api_url_base, api_token):
+    api_url = build_request(api_url_base, FUNCTION_GET_EVENTS, api_token, {'key': 'raids_only', 'value': '1'}, {'key': 'number', 'value': '10'})
     response = requests.get(api_url)
 
     if response.status_code == 200:
+        next_raids = []
         raids = etree.fromstring(response.content.decode('utf-8').replace('\n', ''))
         for raid in raids.find('events'):
             raid_start_timestamp = int(raid.find('start_timestamp').text)
             if (datetime.datetime.fromtimestamp(raid_start_timestamp)) > datetime.datetime.now():
-                return raid
+                next_raids.append(raid)
+        if len(next_raids) > 0:
+            return next_raids
 
     return None
 
@@ -70,19 +73,20 @@ def get_not_signed_in_users(detailed_raid, main_active_chars):
 
 
 def check_next_raid_inscriptions(api_url_base, api_token, check_period, days_check_min=1, days_check_max=2):
-    next_raid = get_next_raid(api_url_base, api_token)
-    if next_raid is not None:
-        next_raid_start_timestamp = int(next_raid.find('start_timestamp').text)
-        next_raid_start_datetime = datetime.datetime.fromtimestamp(next_raid_start_timestamp)
+    next_raids = get_next_raids(api_url_base, api_token)
+    if next_raids is not None:
+        for next_raid in next_raids:
+            next_raid_start_timestamp = int(next_raid.find('start_timestamp').text)
+            next_raid_start_datetime = datetime.datetime.fromtimestamp(next_raid_start_timestamp)
 
-        # Is next raid in less than x days and more than y?
-        if not check_period \
-                or ((next_raid_start_datetime - datetime.timedelta(days=int(days_check_min))) < datetime.datetime.now()
-                    < (next_raid_start_datetime - datetime.timedelta(days=int(days_check_max)))):
-            next_raid_id = next_raid.find('eventid').text
-            det_raid = get_raid_detailed_info(next_raid_id, api_url_base, api_token)
-            main_active_chars = get_main_active_chars(api_url_base, api_token)
-            return {'date': next_raid_start_datetime.strftime('%d/%m/%Y'),
-                    'not_checked_in_users': get_not_signed_in_users(det_raid, main_active_chars)}
+            # Is next raid in less than x days and more than y?
+            if not check_period \
+                    or ((next_raid_start_datetime - datetime.timedelta(days=int(days_check_min))) < datetime.datetime.now()
+                        < (next_raid_start_datetime - datetime.timedelta(days=int(days_check_max)))):
+                next_raid_id = next_raid.find('eventid').text
+                det_raid = get_raid_detailed_info(next_raid_id, api_url_base, api_token)
+                main_active_chars = get_main_active_chars(api_url_base, api_token)
+                return {'date': next_raid_start_datetime.strftime('%d/%m/%Y'),
+                        'not_checked_in_users': get_not_signed_in_users(det_raid, main_active_chars)}
 
     return None
